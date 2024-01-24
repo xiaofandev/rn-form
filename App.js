4165b6ecc41473b6ae302cbf52fe06f3554ac4ad
肖帆 <xiaofandev@126.com>
2024-01-24 14:13:59 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Formik x React Native example
+import React from 'react';
+import { Button, TextInput, View, Text } from 'react-native';
+import { Formik } from 'formik';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const MyReactNativeForm = props => (
+  <Formik
+    initialValues={{ email: '' }}
+    onSubmit={values => console.log(values)}
+  >
+    {({ handleChange, handleBlur, handleSubmit, values }) => (
+      <View>
+        <Text>Email</Text>
+        <TextInput
+          onChangeText={handleChange('email')}
+          onBlur={handleBlur('email')}
+          value={values.email}
+        />
+        <Button onPress={handleSubmit} title="Submit" />
+      </View>
+    )}
+  </Formik>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => (
+  <MyReactNativeForm />
+)
+
+export default App;
